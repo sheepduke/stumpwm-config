64a4788ca3fc503be5f6ad932ca44ed3484b3e39
Danny YUE <sheepduke@gmail.com>
2018-10-30 23:42:41 +0800

    Add volume functionality

--- binding.lisp
+++ binding.lisp
@@ -1,9 +1,30 @@
 (in-package :stumpwm)
 
+;; Run or raise some applications.
 (define-key *root-map* (kbd "f") "exec firefox")
 (define-key *root-map* (kbd "C-f") "firefox")
 (define-key *root-map* (kbd "u") "exec urxvt")
 (define-key *root-map* (kbd "C-u") "urxvt")
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                               Focus                              ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define-key *top-map* (kbd "s-h") "move-focus left")
+(define-key *top-map* (kbd "s-l") "move-focus right")
+(define-key *top-map* (kbd "s-p") "move-focus up")
+(define-key *top-map* (kbd "s-n") "move-focus down")
+
+
 (define-key *top-map* (kbd "s-f") "fullscreen")
-(define-key *top-map* (kbd "s-l") "exec slock")
+(define-key *top-map* (kbd "C-s-l") "exec slock")
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                             Volume                               ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(define-key *top-map* (kbd "s-Up") "volume-set 10")
+(define-key *top-map* (kbd "s-Down") "volume-set -10")
+(define-key *top-map* (kbd "s-Left") "volume-set -2")
+(define-key *top-map* (kbd "s-Right") "volume-set 2")
--- function.lisp
+++ function.lisp
@@ -5,3 +5,27 @@
 
 (defcommand urxvt () ()
   (run-or-raise "urxvt" '(:class "URxvt")))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                             Volume                               ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun volume-get-sinks ()
+  "Return a list containing all available sinks."
+  (let ((result (run-shell-command "pactl list short sinks | ag -o '^[0-9]+'" t)))
+    (str:split #\newline result)))
+
+(defun volume-add (percent)
+  (check-type percent string)
+  (dolist (sink (volume-get-sinks))
+    (run-shell-command
+     (format nil "pactl set-sink-volume ~A ~A" sink percent))))
+
+(defcommand volume-set (percent)
+    ((:number "Enter the percentage"))
+  "Raise the volume for all sinks."
+  (volume-add
+   (format nil "~A~A%"
+           (if (>= percent 0) "+" "")
+           percent)))
