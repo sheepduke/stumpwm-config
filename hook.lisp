ba1707d5428f4b37ed4f1bae869210285a8fa36a
Danny YUE <sheepduke@gmail.com>
2018-11-01 01:30:09 +0800

    [hook] add startup hook

--- hook.lisp
+++ hook.lisp
@@ -4,6 +4,23 @@
 
 (in-package :stumpwm)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                             Startup                              ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defun startup-welcome ()
+  "Show welcome message."
+  (message "All system online.
+Welcome, commander!"))
+
+(setf *start-hook*
+      '(startup-welcome))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                              Window                              ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Make newly created windows full screen.
 (defun new-window-activate-fullscreen (window)
   "Activate full screen for newly created windows."
   (activate-fullscreen window))
