ec916fd4e150ae8469bb46e9d701aecaeb58d38e
YUE Daian <yue_d@worksap.co.jp>
2019-01-07 12:02:28 +0800

    Add message after pausing or resuming dunst

--- src/24-notification.lisp
+++ src/24-notification.lisp
@@ -2,11 +2,13 @@
 
 (defcommand dunst-pause () ()
   (run-shell-command
-   "killall -SIGUSR1 dunst"))
+   "killall -SIGUSR1 dunst")
+  (message "Dunst paused."))
 
 (defcommand dunst-resume () ()
   (run-shell-command
-   "killall -SIGUSR2 dunst"))
+   "killall -SIGUSR2 dunst")
+  (message "Dunst resumed."))
 
 (define-key *top-map* (kbd "s-<") "dunst-pause")
 (define-key *top-map* (kbd "s->") "dunst-resume")
