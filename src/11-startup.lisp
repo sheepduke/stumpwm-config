5d82458672a21641b8afcdd53adcb1bdce713707
YUE Daian <sheepduke@gmail.com>
2020-02-05 12:53:00 +0800

    feat: remove start-up applications

--- src/11-startup.lisp
+++ src/11-startup.lisp
@@ -4,13 +4,6 @@
 
 (in-package :stumpwm)
 
-(run-shell-command "ibus-daemon -dx")
-(run-shell-command "syncthing --no-browser")
-(run-shell-command "pulseaudio --start")
-(run-shell-command "aria2c -D --conf-path=~/.aria2.conf")
-(run-shell-command "unclutter -display $DISPLAY -idle 1")
-(run-shell-command "setxkbmap -option ctrl:nocaps")
-
 ;; Applications
 (run-shell-command "emacs")
 (run-shell-command "urxvt")
